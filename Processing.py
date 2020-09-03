01667c297299e586fbfefb409300137ad6888b84
slowbrain-jaym <jamenator94@gmail.com>
2020-09-03 12:02:45 +0100

    Added mesh 8

--- Processing.py
+++ Processing.py
@@ -7,7 +7,7 @@ import numpy as np
 # number of mesh files in each folder
 root_folder = r"C:\Users\jamen\Google Drive\Everything\Results\P1 Model\OvenMeshIndy\\"
 meshes = [["Mesh0",0,60,1],["Mesh1",0,60,1],["Mesh2",0,60,1],["Mesh3",0,60,1],
-["Mesh4",0,60,1],["Mesh5",0,60,1],["Mesh6",0,60,1],["Mesh7",0,60,1]] 
+["Mesh4",0,60,1],["Mesh5",0,60,1],["Mesh6",0,60,1],["Mesh7",0,60,1],["Mesh8",0,60,1]] 
 # name, first timestep, final timestep, timestep
 areas = ["Food","Inlet","Outlet","Walls"] # prefix for each filename
 
